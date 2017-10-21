8168b51a288af2cdd0c1f3526eaa67700ace7750
sasah <sasahohol2000@gmail.com>
2017-10-22 00:35:17 +0300

    Init

---
+++ src/main/java/com/sasah/revizor/RevizorApplication.java
@@ -0,0 +1,12 @@
+package com.sasah.revizor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevizorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevizorApplication.class, args);
+    }
+}
